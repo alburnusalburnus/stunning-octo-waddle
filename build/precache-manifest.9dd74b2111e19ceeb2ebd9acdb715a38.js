8224daf977be7d44cec31ef2ce5a3c3c5be81d3a
unknown <juha.salakka1@gmail.com>
2020-01-04 20:58:16 +0200

    build

---
+++ build/precache-manifest.9dd74b2111e19ceeb2ebd9acdb715a38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292115ee79d8ee317ade51e617a037c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d14a4a05c7d11b182d98",
+    "url": "/static/css/main.cc29af30.chunk.css"
+  },
+  {
+    "revision": "bdb8bb8017462cb934fb",
+    "url": "/static/js/2.017c34c6.chunk.js"
+  },
+  {
+    "revision": "d14a4a05c7d11b182d98",
+    "url": "/static/js/main.80619f28.chunk.js"
+  },
+  {
+    "revision": "f69f44843943ddfd601e",
+    "url": "/static/js/runtime-main.40e967d0.js"
+  }
+]);
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(morgan("tiny"));
 
 app.use(cors());
 
+app.use(express.static("build"));
+
 const persons = [
   {
     name: "Ada Lovelace",
